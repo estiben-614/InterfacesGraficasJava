38ef47f26819573eca20a70cbc9242327da8db06
Estiben Fernandez Echeverri <efernandeze@unal.edu.co>
2024-04-15 17:31:32 -0500

    Punto 10

---
+++ src/clases/PagoMatricula.java
@@ -0,0 +1,66 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package clases;
+
+public class PagoMatricula {
+    int numeroInscripcion, estratoSocial;
+    String nombres;
+    double patrimonio;
+    
+    public PagoMatricula(){
+        
+    }
+    
+    public PagoMatricula(int numeroInscripcion, int estratoSocial, String nombres, double patrimonio){
+        this.numeroInscripcion=numeroInscripcion;
+        this.estratoSocial=estratoSocial;
+        this.nombres=nombres;
+        this.patrimonio=patrimonio;
+    }
+    
+    public int getNumeroInscripcion(){
+        return numeroInscripcion;
+    }
+    
+    public void setNumeroInscripcion(int numeroInscripcion){
+        this.numeroInscripcion=numeroInscripcion;
+    }
+    
+    public int getEstratoSocial(){
+        return estratoSocial;
+    }
+    
+    public void setEstratoSocial(int estratoSocial){
+        this.estratoSocial=estratoSocial;
+    }
+    
+    public String getNombres(){
+        return nombres;
+    }
+    
+    public void setNombres(String nombres){
+        this.nombres=nombres;
+    }
+    
+    public double getPatrimonio(){
+        return patrimonio;
+    }
+    
+    public void setPatrimonio(double patrimonio){
+        this.patrimonio=patrimonio;
+    }
+    
+    public double calcularPagoMatricula(){
+        double total;
+        if(patrimonio > 2000000 && estratoSocial > 3 ){
+            total = 50000 + patrimonio*0.03;
+        }
+        else {
+            total = 50000;
+        }
+        return total;
+    }
+}
---
+++ src/logica/Punto10.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package logica;
+
+import igu.PantallaPagoMatricula;
+
+public class Punto10 {
+    public static void main(String[] args){
+        PantallaPagoMatricula pantalla = new PantallaPagoMatricula();
+        pantalla.setVisible(true);
+        pantalla.setLocationRelativeTo(null);
+        
+        
+    }
+}
