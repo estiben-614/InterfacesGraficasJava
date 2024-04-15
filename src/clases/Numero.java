dbe10e176863a2722da67ef42179d522e3159c0b
Estiben Fernandez Echeverri <efernandeze@unal.edu.co>
2024-04-15 17:12:05 -0500

    Punto 7

---
+++ src/clases/Numero.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package clases;
+
+public class Numero {
+    double valor;
+    
+    public Numero(){
+        
+    }
+    
+    public Numero(double valor){
+        this.valor=valor;
+    }
+    
+    public double getNumero(){
+        return valor;
+    }
+    
+    public void setNumero(double valor){
+        this.valor=valor;
+    }
+}
